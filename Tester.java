58849d5d9c2b34ffa11dd854f6f1ea8f83502acd
vli21 <vli21@stuy.edu>
2020-11-01 16:12:36 -0500

    tested up to area

---
+++ Tester.java
@@ -0,0 +1,24 @@
+public class Tester{
+  public static void main(String[]args){
+    Point p = new Point (1,3);
+    Point a = new Point (1,3);
+    Point b = new Point (5,3);
+    Point c = new Point (3,7);
+    Triangle abc = new Triangle (a,b,c);
+    
+    /*------------------------------------
+    checks to see if get X and get Y work
+    ------------------------------------*/
+    System.out.println (a.getX());
+    System.out.println (a.getY());
+
+    System.out.println (a.distanceTo(b));
+
+    System.out.println (a.equals(b));
+    System.out.println (a.equals(p));
+    System.out.println (abc.getPerimeter());
+
+    System.out.println (abc.getArea());
+
+  }
+}
